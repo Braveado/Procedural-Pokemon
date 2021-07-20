8910dc7f25a78bfb69e4be6414e79ff492c30f67
Nelson David Martinez Gongora <nelson.martinez.gongora@gmail.com>
2021-07-20 02:34:45 -0500

    pokemon generation done
    -gets a pokemon
    -gets a final evolution
    -gets a variety (filtered)
    -prevents duplicates

--- src/containers/OptionsGenerator.js
+++ src/containers/OptionsGenerator.js
@@ -5,7 +5,7 @@ import PokemonOptions from '../components/PokemonOptions';
 export default function OptionsGenerator() {
   // Constants.
   const apiUrl = 'https://pokeapi.co/api/v2/';
-  const pokemonCount = 210;
+  const pokemonCount = 898;
   const randomRolls = {
     pokemons: 9,
     movesets: 6,
@@ -13,6 +13,12 @@ export default function OptionsGenerator() {
     abilities: 9,
     items: 9
   };
+  const varietiesFilter = [ // Exclude pokemons with this keywords.
+    // Legendaries: forms outside the 600-700 total stats.
+    'black', 'white', 'primal', '10', 'complete', 'ultra', 'crowned', 'eternamax',
+    // Normal: stronger/weaker forms.
+    'mega', 'eternal', 'totem', 'ash', 'solo', 'gmax'
+  ];
 
   // State.
   const [pokemonList, setPokemonList] = useState([]);
@@ -43,14 +49,16 @@ export default function OptionsGenerator() {
   // Geta set of pokemon options.  
   async function getPokemonOptions() {
     setLoading(true);
-
+    
     if(pokemonList.length) {           
-      let pokemons = [];
+      let pokemons = [];      
+      setPokemonOptions(pokemons); 
+
       for (let index = 0; index < randomRolls.pokemons; index++) {
         const pokemon = await getNewPokemon(pokemons)
         pokemons.push(pokemon);
-      }        
-      setPokemonOptions(pokemons);    
+        setPokemonOptions([...pokemons]); 
+      }                 
     }   
      
     setLoading(false);
@@ -61,75 +69,85 @@ export default function OptionsGenerator() {
     let done = false;
     let newPokemonName = '';
 
-    do {
+    do {                
         let pokemon = pokemonList[Math.floor(Math.random()*pokemonList.length)];
+        console.log('initial: '+pokemon.name);
 
-        // WIP
-        // get evolution chain
-        const possiblePokemon = await axios.get(`${apiUrl}pokemon/${pokemon.name}`);
-        const species = await axios.get(possiblePokemon.data.species.url);
+        const initialPokemon = await axios.get(`${apiUrl}pokemon/${pokemon.name}`);
+        const species = await axios.get(initialPokemon.data.species.url);
         const evolutions = await axios.get(species.data.evolution_chain.url);
         
-        // format the chain
+        // Get an array of evolutions.
         let evoChain = [];
         let evoData = evolutions.data.chain;
-        do {
-            // add the starter species in chain first
-            evoChain.push(evoData.species.name);
-
-            // get number of immediate evolutions for that species
+        do {            
+            evoChain.push(evoData.species.name);            
             let numberOfEvolutions = evoData['evolves_to'].length;  
-          
-            // add an array of species names if there are multiple immediate evolutions.
+                      
             if(numberOfEvolutions > 1) {
                 let species = []
                 for (let i = 0; i < numberOfEvolutions; i++) { 
                     species.push(evoData.evolves_to[i].species.name);
                 }
-                evoChain.push(species);
-                // stop the chain
+                evoChain.push(species);                
                 evoData = null;
             }
-            else {
-                // set the next evolution in chain
+            else {                
                 evoData = evoData['evolves_to'][0];
             }
-          
-            // repeat if the chain continues.
-        } while (!!evoData && evoData.hasOwnProperty('evolves_to'));        
+                      
+        } while (!!evoData && evoData.hasOwnProperty('evolves_to')); 
+        console.log('evolutions: '+evoChain);       
+        
+        // Get the/a final evolution.
+        let finalEvolution = evoChain[evoChain.length - 1];
+        if(Array.isArray(finalEvolution)){
+          finalEvolution = finalEvolution[Math.floor(Math.random()*finalEvolution.length)];        
+        }
+        console.log('final evolution: '+finalEvolution);
 
-        // grab the/a final evolution for the chain.
-        let finalPokemon = evoChain[evoChain.length - 1];
-        if(Array.isArray(finalPokemon))
-            finalPokemon = finalPokemon[Math.floor(Math.random()*finalPokemon.length)];
-        // WIP
+        // Get the varieties for the final evolution.
+        const finalSpecies = await axios.get(`${apiUrl}pokemon-species/${finalEvolution}`);
+        let varieties = [];
+        finalSpecies.data.varieties.forEach((v, i) => {
+          varieties.push(finalSpecies.data.varieties[i].pokemon.name)
+        });                
+        console.log('final evolution varieties: '+varieties);
 
+        // Filter varieties for more balance.
+        varieties = varieties.filter(v => {          
+          return !v.split('-').some(keyword => varietiesFilter.includes(keyword))
+        });
+        console.log('filtered varieties: '+varieties);        
+
+        // Get the final pokemon from the varieties.
+        let finalPokemon = varieties[Math.floor(Math.random()*varieties.length)];
+        console.log('final pokemon: '+finalPokemon);
 
         if(!currentPokemons.find(p => p.name === finalPokemon)) {
-            newPokemonName = finalPokemon;
-            done = true;      
+          newPokemonName = finalPokemon;          
+          done = true;      
+          console.log('-----not duplicated: next-----');
         }
-    } while (!done)
-
+        else {
+          console.log('-----DUPLICATED: REROLL-----');
+        }
+    } while (!done)    
     const newPokemon = await axios.get(`${apiUrl}pokemon/${newPokemonName}`);
     return newPokemon.data
   };  
 
   // Render.
   return (  
-    <div className="flex flex-col justify-start items-center bg-gray-100 h-screen w-screen p-8">
+    <div className="flex flex-col justify-start items-center w-full p-8">
         <button 
-            type="button" disabled={loading} onClick={generateOptions}
-            className="bg-gray-900 text-white w-48 p-4 rounded-md hover:bg-gray-600"
+          type="button" disabled={loading} onClick={generateOptions}
+          className="bg-gray-900 text-white w-48 p-4 rounded-md hover:bg-gray-600"
         >
-            Generate Options
+          {!loading ? 'Generate Options' : 'Generating...'}
         </button>
-        <div className="flex flex-col justify-center items-center m-8">
-            {loading ?
-                <div className="bg-white rounded-md p-8">Loading...</div>
-            :
-                <PokemonOptions options={pokemonOptions} />
-            }
+        <div className="flex flex-col justify-center items-center m-8">            
+          <PokemonOptions options={pokemonOptions} />
         </div>      
     </div>     
   );
