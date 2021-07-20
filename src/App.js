f3fc45c7649fad1d7d8ed86f682033b6c11cff55
Nelson David Martinez Gongora <nelson.martinez.gongora@gmail.com>
2021-07-20 02:31:21 -0500

    router and nav links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Navbar from './components/Navbar';
 import OptionsGenerator from './containers/OptionsGenerator';
 
 export default function App() {
   return (  
-    <>     
-      <Navbar />
-      <OptionsGenerator />
-    </>
+    <div className="bg-gray-100 ">     
+      <Router>
+        <Navbar />        
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/generator">
+            <OptionsGenerator />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import {    
+    NavLink,
+    useRouteMatch
+  } from "react-router-dom";
 import logo from '../assets/logo.png';
 
 function Navbar() {
+    let { path, url } = useRouteMatch();
+
     return (
         <div className="flex justify-between items-center w-full h-16 px-32 bg-white" >
             <img src={logo} alt="logo" width="150px" />
             <div className="flex h-full">
-                <button type="button" className="px-4 hover:bg-gray-300">
-                    Home
-                </button>
-                <button type="button" className="px-4 bg-gray-300">
-                    Generator
-                </button>
-                <button type="button" className="px-4 hover:bg-gray-300">
-                    About
-                </button>
+                <NavLink exact to='/'
+                    className="inline-flex items-center justify-center h-full w-24 px-4 border-b-4 border-transparent hover:bg-gray-300"
+                    activeClassName="bg-gray-300 border-gray-600">
+                    <p>Home</p>
+                </NavLink>
+                <NavLink to='/generator' 
+                    className="inline-flex items-center justify-center h-full w-24 px-4 border-b-4 border-transparent hover:bg-gray-300"
+                    activeClassName="bg-gray-300 border-gray-600">
+                    <p>Generator</p>
+                </NavLink>
+                <NavLink to='/about'
+                    className="inline-flex items-center justify-center h-full w-24 px-4 border-b-4 border-transparent hover:bg-gray-300"
+                    activeClassName="bg-gray-300 border-gray-600">
+                    <p>About</p>
+                </NavLink>                
             </div>
         </div>
     )
