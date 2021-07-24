67c7b6acb00f673227988a5ea958719e63233f17
Nelson David Martinez Gongora <nelson.martinez.gongora@gmail.com>
2021-07-24 05:30:16 -0500

    published on github pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {  
-    Link,  
-    NavLink
-  } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from '../assets/logo.png';
 
 function Navbar() {
--- src/components/pokemon/PokemonType.js
+++ src/components/pokemon/PokemonType.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
 export default function PokemonType({type}) {
+    const getColor = () => {
+        switch(type) {
+            case 'normal': return 'bg-normal';
+            case 'fire': return 'bg-fire';
+            case 'water': return 'bg-water';
+            case 'grass': return 'bg-grass';
+            case 'electric': return 'bg-electric';
+            case 'ice': return 'bg-ice';
+            case 'fighting': return 'bg-fighting';
+            case 'poison': return 'bg-poison';
+            case 'ground': return 'bg-ground';
+            case 'flying': return 'bg-flying';
+            case 'psychic': return 'bg-psychic';
+            case 'bug': return 'bg-bug';
+            case 'rock': return 'bg-rock';
+            case 'ghost': return 'bg-ghost';
+            case 'dark': return 'bg-dark';
+            case 'dragon': return 'bg-dragon';
+            case 'steel': return 'bg-steel';
+            case 'fairy': return 'bg-fairy';
+            default: return '';
+        }
+    }
+
     return (
-        <p className={`flex items-center justify-center w-16 capitalize text-sm px-1.5 py-0.5 rounded-md text-white font-semibold bg-${type}`}>
+        <p className={`flex items-center justify-center w-16 capitalize text-sm px-1.5 py-0.5 rounded-md text-white font-semibold ${getColor()}`}>
             {type}
         </p>
     )
