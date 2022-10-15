639409dfa0fd34b6dff8b41714ae030bfd4882ee
Nelson David Martinez Gongora <nelson.martinez.gongora@gmail.com>
2022-10-15 18:42:34 -0500

    Fix footer wrap

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,9 @@ export default function Footer() {
     var braveado = <a href="https://github.com/Braveado" target="_blank" rel="noreferrer" className="text-blue-400 hover:text-blue-500">Nelson Martinez</a>
     var pokeapi = <a href="https://pokeapi.co/" target="_blank" rel="noreferrer" className="text-blue-400 hover:text-blue-500">PokéAPI</a>
     return (
-        <div className="mt-auto flex justify-center items-center w-full bg-white p-4">
-            Created by&nbsp;{braveado}&nbsp;and powered by&nbsp;{pokeapi}. Pokémon and Pokémon character names are trademarks of Nintendo.
+        <div className="mt-auto flex flex-wrap justify-center items-center w-full bg-white p-4">
+            <div className="text-center">Created by&nbsp;{braveado}&nbsp;and powered by&nbsp;{pokeapi}.&nbsp;</div>
+            <div className="text-center">Pokémon and Pokémon character names are trademarks of Nintendo.</div>
         </div>
     )
 }
