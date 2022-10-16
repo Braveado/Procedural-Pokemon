8cc259dd021c51287c967bf44682731151c77c74
Nelson David Martinez Gongora <nelson.martinez.gongora@gmail.com>
2022-10-16 03:05:27 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Footer() {
-    var braveado = <a href="https://github.com/Braveado" target="_blank" rel="noreferrer" className="text-blue-400 hover:text-blue-500">Nelson Martinez</a>
+    var braveado = <a href="https://github.com/Braveado" target="_blank" rel="noreferrer" className="text-blue-400 hover:text-blue-500">Nelson Martínez</a>
     var pokeapi = <a href="https://pokeapi.co/" target="_blank" rel="noreferrer" className="text-blue-400 hover:text-blue-500">PokéAPI</a>
     return (
         <div className="mt-auto flex flex-wrap justify-center items-center w-full bg-white p-4">
